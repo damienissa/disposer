9f20b33154fe7fc7bdc35e0f3acbfd1f46cbc024
Dmytro Virych <dmytro.vi@maxitechs.com>
2021-11-13 11:25:05 +0200

    Updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,107 +1,36 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:example/features/example/data/models/example_data_model.dart';
 import 'package:flutter/material.dart';
 
-import 'features/example/example_module.dart';
+import 'features/app.dart';
+import 'package:disposer/disposer.dart';
+
+import 'features/example/data/datasources/example_data_source.dart';
+import 'features/example/data/repositories/example_data_repository.dart';
+import 'features/example/domain/repositories/example_domain_repository.dart';
+import 'features/example/domain/usecases/example_usecase.dart';
+import 'features/example/presentation/pages/example_page.dart';
+import 'features/example/presentation/view_models/example_view_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ExampleModule.build());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ExampleModule {
+  static ExamplePage build() {
+    final dataSource = ExampleDataSource();
+    final dataRepository = ExampleDataRepository(dataSource);
+    final domainRepository = ExampleDomainRepository(dataRepository);
+    final usecase = ExampleUsecase(domainRepository);
+    final viewModel =
+        ExampleViewModel(usecase); // Can be replaced to a bloc/cubit
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '0',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (c) => ExampleModule.build()));
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return ExamplePage(
+        viewModel: viewModel,
+        willDispose: () {
+          viewModel.dispose();
+        });
   }
 }
